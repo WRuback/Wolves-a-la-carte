964a6df4b990a642e38501bfeaa85da6b383822d
Ezequiel Mejias <ezequiel.mejiasmelo@gmail.com>
2022-01-12 12:45:10 -0500

    Wrote localstorage functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,14 @@ var favoritedItems = [];
 function pullFavorites() {
     var pulledFavorites = JSON.parse(localStorage.getItem("favorites"));
     pulledFavorites !== null? favoritedItems = pulledFavorites : null;
+
+    //render favorites dropdown items
+    return;
+}
+
+function saveFavorites() {
+    localStorage.setItem("favorites", JSON.stringify(favoritedItems));
+    return;
 }
 
 function krogerOAuth(productsArray) {
