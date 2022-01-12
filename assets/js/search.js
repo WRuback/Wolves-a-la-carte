c1995e2e7a5f35bdae3e2263570e6ce29db29361
William Ruback <rubackw@gmail.com>
2022-01-12 10:39:42 -0500

    Added test fetch to SpoonacularAPI

---
+++ assets/js/search.js
@@ -0,0 +1,18 @@
+var spoonKey = "1c81601448cb47bfa0929677d1e9ea44"
+
+function testSpoon(){
+    let url = ` https://api.spoonacular.com/recipes/complexSearch?apiKey=${spoonKey}&query=pasta&maxFat=25&number=2`;
+    fetch(url)
+        .then(function(response){
+            if(response.status === 401)
+            {
+                console.log("You failed!");
+            }
+            return response.json();
+        })
+        .then(function(data){
+            console.log(data);
+        });
+}
+
+testSpoon();
