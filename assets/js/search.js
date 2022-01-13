7d78df643dfbc850a5004638e94ee38407e638fa
William Ruback <rubackw@gmail.com>
2022-01-13 13:38:46 -0500

    Added addfavorites function, and renderfavorites function to index.html

--- assets/js/search.js
+++ assets/js/search.js
@@ -86,7 +86,6 @@ $(function () {
             $("#search-bar").val("");
         })
     });
-
 // --------------- Recipe Render ---------------------
 function renderRecipes(searchResults){
     let display = $("#search-result-display");
